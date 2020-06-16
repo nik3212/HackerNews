d5bd3e5d0e7eacb47e619bcb1863a658e09004d7
Nikita Vasilev <nv3212@gmail.com>
2020-06-17 00:30:46 +0300

    Update snapshot tests

--- HackerNewsTests/SnapshotTests/SettingsShapshotTests.swift
+++ HackerNewsTests/SnapshotTests/SettingsShapshotTests.swift
@@ -1,6 +1,6 @@
 //
 //  SettingsShapshotTests.swift
-//  HackerNewsUITests
+//  HackerNewsTests
 //
 //  Created by Никита Васильев on 10.06.2020.
 //  Copyright © 2020 Никита Васильев. All rights reserved.
@@ -16,15 +16,24 @@ final class SettingsShapshotTests: FBSnapshotTestCase {
     var presenter: SettingsPresenter!
     var themeManager: ThemeManagerProtocol!
     
+    private var identifier: String {
+        let os = UIDevice.current.systemVersion
+        let scale = UIScreen.main.scale
+        return "\(os)_\(scale)"
+    }
+    
     override func setUp() {
         super.setUp()
         
-        recordMode = false//ProcessInfo.processInfo.environment["RECORD_MODE"] == "true"
+        recordMode = ProcessInfo.processInfo.environment["RECORD_MODE"] == "true"
         
         fileNameOptions = [
             FBSnapshotTestCaseFileNameIncludeOption.OS,
-            FBSnapshotTestCaseFileNameIncludeOption.screenScale
+            FBSnapshotTestCaseFileNameIncludeOption.screenScale,
+            FBSnapshotTestCaseFileNameIncludeOption.screenSize
         ]
+        
+        continueAfterFailure = true
         
         settingsViewController = MockContainer().container.resolve(SettingsViewController.self)
         presenter = MockContainer().container.resolve(SettingsPresenter.self, argument: settingsViewController!)
@@ -32,20 +41,14 @@ final class SettingsShapshotTests: FBSnapshotTestCase {
     }
     
     func testThatViewControllerDisplayLightTheme() {
-        let os = UIDevice.current.systemVersion
-        let scale = UIScreen.main.scale
-
         themeManager.theme = .light
         
-        FBSnapshotVerifyView(settingsViewController.view, identifier: "\(os)_\(scale)", perPixelTolerance: 0.05)
+        FBSnapshotVerifyView(settingsViewController.view, identifier: identifier, perPixelTolerance: 0.05)
     }
     
     func testThatViewControllerDisplayDarkTheme() {
-        let os = UIDevice.current.systemVersion
-        let scale = UIScreen.main.scale
-        
         themeManager.theme = .dark
 
-        FBSnapshotVerifyView(settingsViewController.view, identifier: "\(os)_\(scale)", perPixelTolerance: 0.05)
+        FBSnapshotVerifyView(settingsViewController.view, identifier: identifier, perPixelTolerance: 0.05)
     }
 }
